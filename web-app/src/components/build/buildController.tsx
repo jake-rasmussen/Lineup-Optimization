b4405920bd6a3e4d07c7869ff0980aa7943859d3
jrasmus6 <89819505+jrasmus6@users.noreply.github.com>
2025-07-11 21:49:58 -0400

    Added breadcrumbs

--- web-app/src/components/build/buildController.tsx
+++ web-app/src/components/build/buildController.tsx
@@ -7,6 +7,8 @@ import {
   CardFooter,
   CardHeader,
   Divider,
+  Breadcrumbs,
+  BreadcrumbItem,
 } from "@heroui/react";
 import { useEffect, useMemo, useState } from "react";
 import AssignLineup from "./assignLineup";
@@ -141,134 +143,151 @@ const BuildController = ({ handleSubmit }: PropType) => {
   if (!isLoaded) return null;
 
   return (
-    <Card className="h-[90vh] w-full p-4 max-w-7xl shadow-blue-200 shadow-xl border">
-      {step === 0 && (
-        <CardHeader>
-          <div>
-            <h3>Select Players</h3>
-            <p className="text-gray-500 font-normal text-sm">
-              Use the dropdown and search bar below to pick players based on team, year and name.
-              Please select 9 player's season.
-            </p>
-          </div>
-        </CardHeader>
-      )}
-      {step === 1 && (
-        <CardHeader>
-          <div>
-            <h3>Add Constraints</h3>
-            <p className="text-gray-500 font-normal text-sm">
-              Place handedness constraints to control lineup flow.
-            </p>
-          </div>
-        </CardHeader>
-      )}
-      {step === 2 && (
-        <CardHeader>
-          <div>
-            <h3>Select Pitcher Handedness</h3>
-            <p className="text-gray-500 font-normal text-sm">
-              Choose the pitcher’s handedness to simulate matchup splits.
-            </p>
-          </div>
-        </CardHeader>
-      )}
-      {step === 3 && (
-        <CardHeader>
-          <div>
-            <h3>Review & Submit</h3>
-            <p className="text-gray-500 font-normal text-sm">
-              Review your lineup and click submit when ready.
-            </p>
-          </div>
-        </CardHeader>
-      )}
-      <Divider />
-      <CardBody className="max-h-[70vh] overflow-y-scroll flex items-center">
+    <>
+      <Card className="h-[80vh] w-full p-4 max-w-7xl shadow-blue-200 shadow-xl border">
         {step === 0 && (
-          <>
-            {league === League.MLB ? (
-              <SelectPlayersMLB
-                selectedPlayerSeasons={selectedPlayerSeasons}
-                setSelectedPlayerSeasons={setSelectedPlayerSeasons}
-              />
-            ) : (
-              <SelectPlayersALPB
-                selectedPlayerSeasons={selectedPlayerSeasons}
-                setSelectedPlayerSeasons={setSelectedPlayerSeasons}
-              />
-            )}
-          </>
+          <CardHeader>
+            <div>
+              <h3>Select Players</h3>
+              <p className="text-gray-500 font-normal text-sm">
+                Use the dropdown and search bar below to pick players based on team, year and name.
+                Please select 9 player's season.
+              </p>
+            </div>
+          </CardHeader>
         )}
-
         {step === 1 && (
-          <AssignLineup
-            lineup={lineup}
-            setLineup={setLineup}
-            maxConsecutiveHandedness={maxConsecutiveHandedness}
-            setMaxConsecutiveHandedness={setMaxConsecutiveHandedness}
-            selectedPlayerSeasons={selectedPlayerSeasons}
-          />
+          <CardHeader>
+            <div>
+              <h3>Add Constraints</h3>
+              <p className="text-gray-500 font-normal text-sm">
+                Place handedness constraints to control lineup flow.
+              </p>
+            </div>
+          </CardHeader>
         )}
-
         {step === 2 && (
-          <SelectPitcherHandedness
-            pitcherHandedness={pitcherHandedness}
-            setPitcherHandedness={setPitcherHandedness}
-            selectedPlayerSeasons={selectedPlayerSeasons}
-            setSelectedPlayerSeasons={setSelectedPlayerSeasons}
-          />
+          <CardHeader>
+            <div>
+              <h3>Select Pitcher Handedness</h3>
+              <p className="text-gray-500 font-normal text-sm">
+                Choose the pitcher’s handedness to simulate matchup splits.
+              </p>
+            </div>
+          </CardHeader>
         )}
+        {step === 3 && (
+          <CardHeader>
+            <div>
+              <h3>Review & Submit</h3>
+              <p className="text-gray-500 font-normal text-sm">
+                Review your lineup and click submit when ready.
+              </p>
+            </div>
+          </CardHeader>
+        )}
+        <Divider />
+        <CardBody className="max-h-[70vh] overflow-y-scroll flex items-center">
+          {step === 0 && (
+            <>
+              {league === League.MLB ? (
+                <SelectPlayersMLB
+                  selectedPlayerSeasons={selectedPlayerSeasons}
+                  setSelectedPlayerSeasons={setSelectedPlayerSeasons}
+                />
+              ) : (
+                <SelectPlayersALPB
+                  selectedPlayerSeasons={selectedPlayerSeasons}
+                  setSelectedPlayerSeasons={setSelectedPlayerSeasons}
+                />
+              )}
+            </>
+          )}
 
-        {step === 3 && (
-          <ConfirmLineup
-            lineup={lineup}
-            selectedPlayerSeasons={selectedPlayerSeasons}
-            unassignedPlayers={unassignedPlayerIds.map((entry) => `${entry.player.fullName}`)}
-          />
-        )}
-      </CardBody>
-      <Divider />
-      <CardFooter className="flex flex-row gap-2">
-        <Button
-          color="danger"
-          variant="light"
-          onPress={step === 0 ? undefined : prevStep}
-          isDisabled={step === 0}
-        >
-          Back
-        </Button>
-        {step < 3 && (
+          {step === 1 && (
+            <AssignLineup
+              lineup={lineup}
+              setLineup={setLineup}
+              maxConsecutiveHandedness={maxConsecutiveHandedness}
+              setMaxConsecutiveHandedness={setMaxConsecutiveHandedness}
+              selectedPlayerSeasons={selectedPlayerSeasons}
+            />
+          )}
+
+          {step === 2 && (
+            <SelectPitcherHandedness
+              pitcherHandedness={pitcherHandedness}
+              setPitcherHandedness={setPitcherHandedness}
+              selectedPlayerSeasons={selectedPlayerSeasons}
+              setSelectedPlayerSeasons={setSelectedPlayerSeasons}
+            />
+          )}
+
+          {step === 3 && (
+            <ConfirmLineup
+              lineup={lineup}
+              selectedPlayerSeasons={selectedPlayerSeasons}
+              unassignedPlayers={unassignedPlayerIds.map((entry) => `${entry.player.fullName}`)}
+            />
+          )}
+        </CardBody>
+        <Divider />
+        <CardFooter className="flex flex-row gap-2">
           <Button
-            color="primary"
-            onPress={nextStep}
-            isDisabled={
-              selectedPlayerSeasons.length !== 9 ||
-              selectedPlayerSeasons.some((ps) => !ps.season) ||
-              (step === 1 && isConstraintImpossible)
-            }
+            color="danger"
+            variant="light"
+            onPress={step === 0 ? undefined : prevStep}
+            isDisabled={step === 0}
           >
-            Next
+            Back
           </Button>
-        )}
-        {step === 3 && (
-          <Button
-            onPress={() => {
-              handleSubmit(lineup, unassignedPlayerIds, selectedPlayerSeasons, maxConsecutiveHandedness);
+          {step < 3 && (
+            <Button
+              color="primary"
+              onPress={nextStep}
+              isDisabled={
+                selectedPlayerSeasons.length !== 9 ||
+                selectedPlayerSeasons.some((ps) => !ps.season) ||
+                (step === 1 && isConstraintImpossible)
+              }
+            >
+              Next
+            </Button>
+          )}
+          {step === 3 && (
+            <Button
+              onPress={() => {
+                handleSubmit(lineup, unassignedPlayerIds, selectedPlayerSeasons, maxConsecutiveHandedness);
+              }}
+              color="primary"
+            >
+              Submit
+            </Button>
+          )}
+          {step === 0 && selectedPlayerSeasons.length > 0 && (
+            <div className="grow flex justify-end items-center">
+              {`Selected Players: ${selectedPlayerSeasons.length}/9`}
+            </div>
+          )}
+        </CardFooter>
+      </Card>
+
+      {/* Breadcrumbs below the card */}
+      <Breadcrumbs className="mt-8 mx-auto w-full flex items-center justify-center dark">
+        {["Select Players", "Add Constraints", "Pitcher Handedness", "Review & Submit"].map((label, index) => (
+          <BreadcrumbItem
+            key={index}
+            isCurrent={step === index}
+            onClick={() => {
+              if (selectedPlayerSeasons.length === 9) setStep(index);
             }}
-            color="primary"
+            className={`cursor-pointer ${selectedPlayerSeasons.length !== 9 ? "text-white cursor-not-allowed" : ""}`}
           >
-            Submit
-          </Button>
-        )}
-
-        {step === 0 && selectedPlayerSeasons.length > 0 && (
-          <div className="grow flex justify-end items-center">
-            {`Selected Players: ${selectedPlayerSeasons.length}/9`}
-          </div>
-        )}
-      </CardFooter>
-    </Card>
+            {label}
+          </BreadcrumbItem>
+        ))}
+      </Breadcrumbs>
+    </>
   );
 };
 
